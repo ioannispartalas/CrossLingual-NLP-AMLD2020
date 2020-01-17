f3894a06dbc31ae7ed4b9025fb124dbd597e6dfe
Ioannis Partalas <ipartalas@expedia.com>
2020-01-17 08:17:03 -0800

    Added script for installing LASER

---
+++ install_laser.sh
@@ -0,0 +1,14 @@
+echo "Checking out github repo"
+INSTALL_PATH="${HOME}/projs"
+echo ${INSTALL_PATH}
+mkdir "${INSTALL_PATH}"
+cd "${INSTALL_PATH}"
+git clone https://github.com/facebookresearch/LASER.git
+
+cd "${INSTALL_PATH}/LASER/"
+export LASER="${INSTALL_PATH}/LASER"
+
+pip install transliterate
+
+./install_models.sh
+./install_external_tools.sh

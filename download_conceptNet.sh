df8ff0ab8ea48232cb926c70652b3044742a5ea9
Ioannis Partalas <ipartalas@expedia.com>
2020-01-17 03:05:44 -0800

    Fixed issues in extract script

--- download_conceptNet.sh
+++ download_conceptNet.sh
@@ -3,9 +3,8 @@ wget "https://conceptnet.s3.amazonaws.com/downloads/2019/numberbatch/numberbatch
 echo "Embeddings downloaded. Start unzipping."
 gunzip numberbatch-19.08.txt.gz
 echo "Embeddings unzipped. Start filtering English and Spanish words."
-python extract_embeddings_conceptNet.py
+python extract_embeddings.py
 echo "Done. Cleaning.."
-rm numberbatch-19.08.txt
 echo "Done!"
 echo 
 echo
